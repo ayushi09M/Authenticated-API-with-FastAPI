4d5500d0871f20c13ce4b61984fb3c003bf97b4a
Ayushi <ayushi99malviya@gmail.com>
2025-09-02 21:35:10 +0530

    Fast API practical learning

--- My_Project/Product/database.py
+++
@@ -1,9 +0,0 @@
-from sqlalchemy import create_engine, engine
-from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy.orm import sessionmaker
-
-# refernce to the database path product.db in the current directory
-SQLALCHEMY_DATABASE_URL = "sqlite:///./product.db"
-engine = create_engine(SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False})
-SessionLocal = sessionmaker(bind=engine, autocommit=False, autoflush=False)
-Base = declarative_base()
